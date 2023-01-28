42fcb564ff27dbf93864e7fc44c0e14514e1c20a
chaud1024 <chaud1024@gmail.com>
2023-01-28 15:25:06 +0900

    add lottie file into About

--- components/templates/About.tsx
+++ components/templates/About.tsx
@@ -2,9 +2,37 @@ import styled from "@emotion/styled";
 import { Flex, Title, Text } from "@mantine/core";
 import Image from "next/image";
 import { arsenal } from "types/TextType";
+import { create } from "@lottiefiles/lottie-interactivity";
 import { useEffect, useRef } from "react";
 
 const About = () => {
+  const containerRef: any = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    import("@lottiefiles/lottie-player");
+  });
+  useEffect(() => {
+    containerRef.current.addEventListener(
+      "load",
+      (e: React.ChangeEvent<HTMLDivElement>) => {
+        // configure the interactivity library
+        console.log("load");
+        create({
+          mode: "scroll",
+          player: "#firstLottie",
+          actions: [
+            {
+              visibility: [0, 1],
+              type: "seek",
+              frames: [0, 120],
+            },
+          ],
+        });
+      }
+    );
+
+    return () => {};
+  }, [containerRef]);
   return (
     <Wrap>
       <Flex
@@ -30,6 +58,15 @@ const About = () => {
         <div>
           현재 포트폴리오는 React, Next.js, Mantine(emotion)으로 제작했습니다.
         </div>
+        <lottie-player
+          id="firstLottie"
+          ref={containerRef}
+          autoplay
+          loop
+          mode="normal"
+          src="https://assets1.lottiefiles.com/packages/lf20_bevi1628.json"
+          style={{ width: "450px", height: "450px" }}
+        ></lottie-player>
       </div>
     </Wrap>
   );
