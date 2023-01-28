4b92a9e3af2c364e161be647f2dcf8cfb667d114
chaud1024 <chaud1024@gmail.com>
2023-01-28 14:01:41 +0900

    add text type

---
+++ types/TextType.ts
@@ -0,0 +1,19 @@
+import { Montserrat, Arsenal, IBM_Plex_Sans_KR } from "@next/font/google";
+
+export const ibmKr = IBM_Plex_Sans_KR({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+
+export const montserrat = Montserrat({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+
+export const arsenal = Arsenal({
+  weight: ["700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
