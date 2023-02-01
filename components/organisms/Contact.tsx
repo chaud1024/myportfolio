d3d7857efad58b464d6d5a9964ff719f25ad63fe
chaud1024 <chaud1024@gmail.com>
2023-02-02 00:17:03 +0900

    edit contact infomation

--- components/organisms/Contact.tsx
+++ components/organisms/Contact.tsx
@@ -1,33 +1,56 @@
-import { Box, Flex, Text, Title } from "@mantine/core";
-import { arsenal, montserrat } from "types/TextType";
+import { Box, Flex, createStyles } from "@mantine/core";
 import {
   IconBrandGithub,
   IconMailForward,
-  IconPhoneCall,
+  IconDeviceMobile,
 } from "@tabler/icons-react";
 
 const Contact = () => {
+  const { classes } = useStyles();
   return (
     <>
-      <Title className={arsenal.className}>Contact Me</Title>
-      <Box>
-        <Flex gap={8}>
-          <IconPhoneCall size={24} stroke={2} />
-          <Text className={montserrat.className}>+82 10 9019 1865</Text>
-        </Flex>
-        <Flex gap={8}>
-          <IconMailForward size={24} stroke={2} />
-          <Text className={montserrat.className}>chaud1024@gmail.com</Text>
-        </Flex>
-        <Flex gap={8}>
-          <IconBrandGithub size={24} stroke={2} />
-          <Text className={montserrat.className}>
-            https://github.com/chaud1024
-          </Text>
-        </Flex>
+      <Box className={classes.wrap}>
+        <Box className={classes.wrapIcon}>
+          <a href="tel: +82-10-9019-1865">
+            <IconDeviceMobile size={32} stroke={2} />
+          </a>
+        </Box>
+        <Box className={classes.wrapIcon}>
+          <a href="mailto: chaud1024@gmail.com">
+            <IconMailForward size={32} stroke={2} />
+          </a>
+        </Box>
+        <Box className={classes.wrapIcon}>
+          <a
+            href="https://github.com/chaud1024"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <IconBrandGithub size={32} stroke={2} />
+          </a>
+        </Box>
       </Box>
     </>
   );
 };
 
 export default Contact;
+
+const useStyles = createStyles((theme, _params) => ({
+  wrap: {
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.spacing.md,
+  },
+  wrapIcon: {
+    padding: "12px 12px 6px",
+    borderRadius: theme.spacing.xs,
+    ["&:hover"]: {
+      background:
+        "linear-gradient(61deg, rgba(237,110,160,1) 0%, rgba(255,156,117,1) 79%)",
+      ["svg"]: {
+        stroke: theme.white,
+      },
+    },
+  },
+}));
