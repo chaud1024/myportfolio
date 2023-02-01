6b9e3c5d5e58c31c8b50e6c5a010b0654945fe2b
chaud1024 <chaud1024@gmail.com>
2023-02-02 00:16:46 +0900

    edit profile image

--- components/organisms/AboutProfile.tsx
+++ components/organisms/AboutProfile.tsx
@@ -8,10 +8,10 @@ const AboutProfile = () => {
       <Title className={arsenal.className}>Im Bora, Front-end Developer</Title>
 
       <Image
-        src="https://images.unsplash.com/photo-1617037448248-6bd7b4a0d038?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+        src="/img/Bora_Kim.jpg"
         alt="profile shot"
         width={500}
-        height={600}
+        height={640}
       />
     </>
   );
