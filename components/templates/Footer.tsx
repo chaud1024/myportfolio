126ad1b30daec603c39aa5c9de13eecc7f5f0879
chaud1024 <chaud1024@gmail.com>
2023-02-02 00:17:10 +0900

    edit footer

--- components/templates/Footer.tsx
+++ components/templates/Footer.tsx
@@ -1,16 +1,69 @@
 import { useWindowScroll } from "@mantine/hooks";
-import { Affix, Button, Text, Transition } from "@mantine/core";
-import styled from "@emotion/styled";
+import {
+  Affix,
+  Box,
+  Button,
+  Flex,
+  Text,
+  Transition,
+  createStyles,
+} from "@mantine/core";
 import { montserrat } from "types/TextType";
+import {
+  IconBrandGithub,
+  IconMailForward,
+  IconDeviceMobile,
+} from "@tabler/icons-react";
+import Link from "next/link";
 
 const Footer = () => {
   const [scroll, scrollTo] = useWindowScroll();
+  const { classes } = useStyles();
 
   return (
-    <WrapFooter>
-      <Text align="center">
-        Affix is located at the bottom of the screen, scroll to see it
-      </Text>
+    <Box className={classes.wrapFooter}>
+      <Box>
+        <Box className={classes.wrapThanks}>
+          <Text className={montserrat.className}>
+            Thanks for visiting My portfolio.
+          </Text>
+          <Text className={montserrat.className}>
+            I am looking forward to hearing from you.
+          </Text>
+        </Box>
+        <Flex gap={"xl"} justify={"end"}>
+          <Flex gap={8}>
+            <IconDeviceMobile size={24} stroke={1} />
+            <Text className={montserrat.className}>
+              <a href="tel: +82-10-9019-1865">+82 10 9019 1865</a>
+            </Text>
+          </Flex>
+          <Flex gap={8}>
+            <IconMailForward size={24} stroke={1} />
+            <Text className={montserrat.className}>
+              <a href="mailto: chaud1024@gmail.com">chaud1024@gmail.com</a>
+            </Text>
+          </Flex>
+          <Flex gap={8}>
+            <IconBrandGithub size={24} stroke={1} />
+            <Text className={montserrat.className}>
+              <Link href="https://github.com/chaud1024" target="_blank">
+                https://github.com/chaud1024
+              </Link>
+            </Text>
+          </Flex>
+        </Flex>
+        <Box>
+          <Text
+            align="right"
+            fz={"sm"}
+            mt={"lg"}
+            className={montserrat.className}
+          >
+            Copyright @Bora Kim
+          </Text>
+        </Box>
+      </Box>
       <Affix position={{ bottom: 20, right: 20 }}>
         <Transition transition="slide-up" mounted={scroll.y > 0}>
           {(transitionStyles) => (
@@ -27,13 +80,22 @@ const Footer = () => {
           )}
         </Transition>
       </Affix>
-    </WrapFooter>
+    </Box>
   );
 };
 
 export default Footer;
 
-const WrapFooter = styled.div({
-  width: "100%",
-  padding: "32px",
-});
+const useStyles = createStyles((theme, _params) => ({
+  wrapFooter: {
+    width: "100%",
+    padding: "32px",
+  },
+
+  wrapThanks: {
+    marginBottom: "32px",
+    paddingBottom: "32px",
+    borderBottom: "1px solid",
+    borderColor: theme.colors.dark[0],
+  },
+}));
