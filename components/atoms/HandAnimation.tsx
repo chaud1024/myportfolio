864cc82b6cf9691078120360b9ba9b25155cc3e0
chaud1024 <chaud1024@gmail.com>
2023-02-02 00:16:30 +0900

    add hand animation component

---
+++ components/atoms/HandAnimation.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "@emotion/styled";
+const HandAnimation = (props: any) => (
+  <Wave
+    className="emoji"
+    role="img"
+    aria-label={props.label ? props.label : ""}
+    aria-hidden={props.label ? "false" : "true"}
+  >
+    {props.symbol}
+  </Wave>
+);
+export default HandAnimation;
+
+const Wave = styled.span`
+  font-size: 60px;
+  animation-name: wave-animation; /* Refers to the name of your @keyframes element below */
+  animation-duration: 2.5s; /* Change to speed up or slow down */
+  animation-iteration-count: infinite; /* Never stop waving :) */
+  transform-origin: 70% 70%; /* Pivot around the bottom-left palm */
+  display: inline-block;
+  @keyframes wave-animation {
+    0% {
+      transform: rotate(0deg);
+    }
+    10% {
+      transform: rotate(14deg);
+    } /* The following five values can be played with to make the waving more or less extreme */
+    20% {
+      transform: rotate(-8deg);
+    }
+    30% {
+      transform: rotate(14deg);
+    }
+    40% {
+      transform: rotate(-4deg);
+    }
+    50% {
+      transform: rotate(10deg);
+    }
+    60% {
+      transform: rotate(0deg);
+    } /* Reset for the last half to pause */
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+`;
