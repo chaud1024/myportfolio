3a362e009f80978d8ed3c9774fa0c855bb3b06e1
chaud1024 <chaud1024@gmail.com>
2023-02-08 21:32:26 +0900

    add getStaticProps(locale)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,13 @@
 import Layout from "../layouts/Layout";
+import { serverSideTranslations} from "next-i18next/serverSideTranslations"
+
+export async function getStaticProps({locale}: {locale: string}) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['home', 'experience', 'project']))
+    }
+  }
+}
 
 export default function Home() {
   return (
